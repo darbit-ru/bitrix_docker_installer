d7dfadead53a3f17e6832e7f9eeef181c5837fb5
Marat Sultanayev <m.sultanayev@gmail.com>
2024-07-17 13:59:19 +0500

    fixes

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 set -e
 
 REPO_PATH=https://github.com/darbit-ru/bitrix_docker_source_phmk.git
+REPO_FOLDER=bitrix_docker_source_phmk
 BITRIX_SETUP_PATH=http://www.1c-bitrix.ru/download/scripts/bitrixsetup.php
 BITRIX_RESTORE_PATH=http://www.1c-bitrix.ru/download/scripts/restore.php
 GROUP_NAME=www-data
@@ -72,12 +73,15 @@ then
   echo -e "\e[33mCloning repo to local... \e[39m"
   cd $WORK_PATH && \
   git clone $REPO_PATH > /dev/null 2>&1 && \
+  mv $WORK_PATH/$REPO_FOLDER/* $DOCKER_FOLDER_PATH > /dev/null 2>&1 && \
+  rm -rf $WORK_PATH/$REPO_FOLDER && \
   cd $(dirname $WORK_PATH) && chmod -R 775 $(basename $WORK_PATH) && chown -R root:www-data $(basename $WORK_PATH) && \
   cd $DOCKER_FOLDER_PATH
   echo -e "\e[32m    Done \e[39m\n"
 
   # copy .env file from template file
   echo -e "\e[33mCopy environment setting file and starting configuration \e[39m"
+  cd $DOCKER_FOLDER_PATH
   cp -f .env_template .env && \
   echo -e "\e[32m    Done \e[39m\n"
 
@@ -195,7 +199,8 @@ then
   # starting docker containers
   cd $DOCKER_FOLDER_PATH
   echo -e "\n\e[33mStarting DOCKER containers...\e[39m"
-  docker-compose up -d > /dev/null 2>&1
+  #docker-compose up -d > /dev/null 2>&1
+  docker-compose up -d
   echo -e "\e[32m    Started\e[39m\n"
 
   #
